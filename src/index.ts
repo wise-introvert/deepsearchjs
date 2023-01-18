abf4e04e837f5339d4223e5bd04086fada25e9f7
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-01-18 17:24:36 -0500

    docs: add jsdoc generated using chatgpt

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
+/**
+ * This code exports three functions: 
+ * 1. isObject
+ * 2. isArray
+ * 3. search
+ * 
+ * isObject takes any value and returns a 
+ * boolean indicating whether it is an object. 
+ * 
+ * isArray takes any value and returns a 
+ * boolean indicating whether it is an array. 
+ * 
+ * search takes an object and a query string and 
+ * returns an object containing the search results. getkeys is a helper function
+ */
+
 import { get, isEmpty } from "lodash";
 
+/**
+ * Check if the provided value is an object
+ * @param x - value to check
+ * @returns - boolean indicating whether the provided value is an object
+ */
 export const isObject = (x: any): boolean => {
   return Object.prototype.toString.call(x) === "[object Object]";
 };
 
+/**
+ * Check if the provided value is an array
+ * @param x - value to check
+ * @returns - boolean indicating whether the provided value is an array
+ */
 export const isArray = (x: any): boolean => Array.isArray(x);
 
+/**
+ * Get all the keys of an object and its nested objects/arrays
+ * @param obj - object to get the keys from
+ * @param prefix - prefix to add to the keys, defaults to empty string
+ * @returns - array of keys
+ */
 export const getkeys = (obj: any, prefix?: any): any => {
   try {
     const keys: string[] = Object.keys(obj);
@@ -31,6 +63,12 @@ export const getkeys = (obj: any, prefix?: any): any => {
   }
 };
 
+/**
+ * Search an object and its nested objects/arrays for the provided query
+ * @param obj - object to search
+ * @param query - string to search for
+ * @returns - object containing the search results
+ */
 export const search = <T = any>(obj: T, query: string): any => {
   const keys: string[] = getkeys(obj);
 
