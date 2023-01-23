439395d769d6ae3e3b766c27a9d740d360d9bf9e
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-01-23 02:02:06 -0500

    test: refactor test to match updated search function parameters

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,8 +1,63 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, test, it, expect } from "@jest/globals";
 
 import { search } from "../src";
 
 describe("search", () => {
+  const testObj = {
+    foo: "bar",
+    nested: {
+      key1: "value1",
+      key2: "value2",
+    },
+    arr: [{ a: 1 }, { b: 2 }],
+  };
+
+  test("searching for a string query", () => {
+    const query = "key1";
+    const result = search(testObj, query);
+    expect(result).toEqual({ "nested.key1": "value1" });
+  });
+
+  test("searching for a regular expression query", () => {
+    const query = /key/gi;
+    const result = search(testObj, query);
+    console.log(result);
+    expect(result).toEqual({
+      "nested.key1": "value1",
+      "nested.key2": "value2",
+    });
+  });
+
+  test("searching with a function query", () => {
+    const query = (key: string, value: any) =>
+      key.endsWith("1") && value === "value1";
+    const result = search(testObj, query);
+    expect(result).toEqual({ "nested.key1": "value1" });
+  });
+
+  test("searching for a non-existent query", () => {
+    const query = "nonExistentKey";
+    const result = search(testObj, query);
+    expect(result).toEqual({});
+  });
+
+  test("searching in array", () => {
+    const query = "a";
+    const result = search(testObj, query);
+    expect(result).toEqual({ "arr[0].a": 1 });
+  });
+});
+
+describe("search", () => {
+  const testObj = {
+    foo: "bar",
+    nested: {
+      key1: "value1",
+      key2: "value2",
+    },
+    arr: [{ a: 1 }, { b: 2 }],
+  };
+
   it("returns an object containing the search results", () => {
     const obj = {
       a: {
@@ -30,5 +85,39 @@ describe("search", () => {
     const query = "x";
     const result = search(obj, query);
     expect(result).toEqual({});
+  });
+
+  test("searching for a string query", () => {
+    const query = "key1";
+    const result = search(testObj, query);
+    expect(result).toEqual({ "nested.key1": "value1" });
+  });
+
+  test("searching for a regular expression query", () => {
+    const query = /key/gi;
+    const result = search(testObj, query);
+    expect(result).toEqual({
+      "nested.key1": "value1",
+      "nested.key2": "value2",
+    });
+  });
+
+  test("searching with a function query", () => {
+    const query = (key: string, value: any) =>
+      key.endsWith("1") && value === "value1";
+    const result = search(testObj, query);
+    expect(result).toEqual({ "nested.key1": "value1" });
+  });
+
+  test("searching for a non-existent query", () => {
+    const query = "nonExistentKey";
+    const result = search(testObj, query);
+    expect(result).toEqual({});
+  });
+
+  test("searching in array", () => {
+    const query = "a";
+    const result = search(testObj, query);
+    expect(result).toEqual({ "arr[0].a": 1 });
   });
 });
